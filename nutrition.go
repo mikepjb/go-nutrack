a2cc3787edc898db9fc44dee444a4777cdcc80e5
Michael Bruce <me@michaelbruce.co>
2019-03-10 10:05:50 +0000

    calculate total price for a recipe

--- nutrition.go
+++ nutrition.go
@@ -2,10 +2,27 @@ package main
 
 import "fmt"
 
-type Recipe struct{}
+type Recipe struct {
+	ingredients []Ingredient
+}
 
-func (Recipe) Price() float32 {
-	return 0.7
+func (r Recipe) Price() float32 {
+	var price float32
+
+	for _, i := range r.ingredients {
+		price += i.Price()
+	}
+
+	return price
+}
+
+type Ingredient struct {
+	name  string
+	price float32 // price per 100g
+}
+
+func (i Ingredient) Price() float32 {
+	return i.price
 }
 
 func main() {
--- nutrition_test.go
+++ nutrition_test.go
@@ -1,15 +1,24 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestCalculatesTheCostOfPorridge(t *testing.T) {
-	// ingredients := []Ingredients{}}
-	porridge := Recipe{}
+	oats := Ingredient{"oats", 0.7}
+	blueberries := Ingredient{"blueberries", 0.2}
+
+	porridge := Recipe{[]Ingredient{oats}}
 
 	if porridge.Price() != 0.7 {
-		fmt.Errorf("wrong price for porridge: %v\n", porridge.Price())
+		t.Errorf("wrong price for porridge: %v\n", porridge.Price())
+	}
+
+	porridgeWithBlueberries := Recipe{[]Ingredient{oats, blueberries}}
+
+	if porridgeWithBlueberries.Price() != 0.9 {
+		t.Errorf(
+			"wrong price for porridge with blueberries: %v\n",
+			porridgeWithBlueberries.Price())
 	}
 }

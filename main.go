3e07542811f604e669af7b88199208ecd7009ce3
Michael Bruce <me@michaelbruce.co>
2019-03-23 08:23:22 +0000

    prettier printing

--- main.go
+++ main.go
@@ -8,6 +8,41 @@ import (
 	"github.com/mikepjb/nutrition/src/nutrition"
 )
 
+func PrintOrders(orders []nutrition.Order) {
+	fmt.Printf("Orders:\n==================\n")
+	for _, o := range orders {
+		fmt.Printf("%v:\n", o.Name)
+		for _, r := range o.Recipes {
+			fmt.Printf("  %v\n", r.Name)
+		}
+	}
+	fmt.Printf("\n")
+}
+
+func PrintRecipes(recipes []nutrition.Recipe) {
+	fmt.Printf("Recipes:\n==================\n")
+	for _, r := range recipes {
+		fmt.Printf("%v: %v\n", r.Name, r.Price())
+	}
+	fmt.Printf("\n")
+}
+
+func PrintIngredients(ingredients []nutrition.Ingredient) {
+	fmt.Printf("Ingredients:\n==================\n")
+	for _, i := range ingredients {
+		fmt.Printf("%v (%vg): %v\n", i.Name, i.Amount, i.Price())
+	}
+	fmt.Printf("\n")
+}
+
+func PrintFoodItems(items []nutrition.FoodItem) {
+	fmt.Printf("FoodItems:\n==================\n")
+	for _, i := range items {
+		fmt.Printf("%v: %v\n", i.Desc, i.Price)
+	}
+	fmt.Printf("\n")
+}
+
 func main() {
 	if len(os.Args[1:]) == 0 {
 		fmt.Println("no args supplied")
@@ -17,16 +52,19 @@ func main() {
 	jsonPath := strings.Join(os.Args[1:], "")
 	orders, recipes, ingredients, foodItems := nutrition.ReadFile(jsonPath)
 
-	fmt.Printf("Orders: %v\n", orders)
-	fmt.Printf("Recipes: %v\n", recipes)
-	fmt.Printf("Ingredients: %v\n", ingredients)
-	fmt.Printf("FoodItems: %v\n", foodItems)
+	PrintOrders(orders)
+	PrintRecipes(recipes)
+	PrintIngredients(ingredients)
+	PrintFoodItems(foodItems)
 
 	var totalRecipePrices float32
 
-	for _, r := range recipes {
-		totalRecipePrices += r.Price()
+	for _, o := range orders {
+		for _, r := range o.Recipes {
+			fmt.Printf("Price for %v is %v\n", r.Name, r.Price())
+			totalRecipePrices += r.Price()
+		}
 	}
 
-	fmt.Printf("Total Prices for Recipes: %v\n", totalRecipePrices)
+	fmt.Printf("Total Prices for Orders: %v\n", totalRecipePrices)
 }

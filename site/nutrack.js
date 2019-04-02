b5fdc76063531c828b7e3bf776af243fa2fc771a
Michael Bruce <me@michaelbruce.co>
2019-04-02 21:55:50 +0100

    better today's menu cards

--- site/nutrack.js
+++ site/nutrack.js
@@ -62,6 +62,13 @@ function todaysMenu(orders) {
     div.classList.add("energy")
     rdiv.appendChild(div)
 
+    var div = document.createElement("div")
+    // var text = document.createTextNode("View Recipe")
+    div.innerHTML = "View Recipe <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' class='icon-cheveron-right'><path class='secondary' d='M10.3 8.7a1 1 0 0 1 1.4-1.4l4 4a1 1 0 0 1 0 1.4l-4 4a1 1 0 0 1-1.4-1.4l3.29-3.3-3.3-3.3z'/></svg>"
+    // div.appendChild(text)
+    div.classList.add("view-recipe")
+    rdiv.appendChild(div)
+
     menuSection.appendChild(rdiv)
   })
 }
